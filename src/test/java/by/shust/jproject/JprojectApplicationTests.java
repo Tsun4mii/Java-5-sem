034e996b381724f0aae235de95a8195c57aa3823
Yuri Shust <shustts@yandex.by>
2021-09-24 00:12:21 +0300

    OAuth authorization

---
+++ src/test/java/by/shust/jproject/JprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package by.shust.jproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
