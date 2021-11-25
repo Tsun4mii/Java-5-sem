00f2d065766ac5036cb82ffab4326cbb741ef1e1
Yuri Shust <shustts@yandex.by>
2021-11-25 07:53:59 +0300

    Update 2

--- src/main/java/bstu/shust/jprojectv2/dto/NameRequest.java
+++ src/main/java/bstu/shust/jprojectv2/dto/NameRequest.java
@@ -2,11 +2,9 @@ package bstu.shust.jprojectv2.dto;
 
 public class NameRequest {
     private String name;
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
--- src/main/java/bstu/shust/jprojectv2/security/JwtFactory.java
+++ src/main/java/bstu/shust/jprojectv2/security/JwtFactory.java
@@ -1,4 +1,5 @@
 package bstu.shust.jprojectv2.security;
 
 public class JwtFactory {
+    public void hello_there(){};
 }
